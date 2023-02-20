397d500b6ca600979fbb739bb318899962fc6d27
Ali Aghaeifar <ali.aghaeifar@tuebingen.mpg.de>
2023-02-20 15:25:12 +0100

    update to read new header

--- outputs/read_output.m
+++ outputs/read_output.m
@@ -1,27 +1,38 @@
 clear
-fname = 'results.dat';
+clc
+rad_ref_um = 53.367;
+fname{1} = './results_gre_fieldmap1.dat';
+fname{2} = './results_gre_fieldmap2.dat';
 
-fileID = fopen(fname);
-n_spins         = fread(fileID, 1, 'int32=>int32');
-n_fieldmaps     = fread(fileID, 1, 'int32=>int32');
-n_sample_length = fread(fileID, 1, 'int32=>int32');
-device_count    = fread(fileID, 1, 'int32=>int32');
-m_xyz = fread(fileID, 3 * n_spins * n_fieldmaps * n_sample_length * device_count, 'single=>single');
-fclose(fileID);
-m_xyz = reshape(m_xyz, [3, n_spins, n_fieldmaps, n_sample_length, device_count]);
-m_xyz = permute(m_xyz, [1 2 5 3 4]);
-m_xyz = reshape(m_xyz, [3, n_spins*device_count, n_fieldmaps, n_sample_length]);
+% fname{1} = './outputs/results_ssfp_fieldmap1.dat';
+% fname{2} = './outputs/results_ssfp_fieldmap2.dat';
 
-figure
-spins_xy = squeeze(complex(sum(m_xyz(1,:,:,:), 2), sum(m_xyz(2,:,:,:), 2) ));
+spins_xy = [];
+for i=1:numel(fname)
+    fileID = fopen(fname{i});
+    hdr_size  = fread(fileID, 1, 'int32=>int32');
+    dims      = fread(fileID, 4, 'int32=>int32'); % 3 * n_spins * n_sample_length_scales * device_count
+    if dims(3) ~= hdr_size/4-numel(dims)
+        warning('Why header info is confusing here?')
+    end
+    scales    = fread(fileID, hdr_size/4-numel(dims), 'single=>single');
+    m_xyz = fread(fileID, prod(dims), 'single=>single');
+    fclose(fileID);
+    m_xyz = reshape(m_xyz, dims(:)');
+    m_xyz = permute(m_xyz, [1 2 4 3]);
+    m_xyz = reshape(m_xyz, [dims(1), dims(2)*dims(4), dims(3)]);
+    spins_xy = cat(4, spins_xy, m_xyz);
+end
+
+spins_xy = squeeze(complex(sum(spins_xy(1,:,:,:), 2), sum(spins_xy(2,:,:,:), 2) ));
+
+%figure
 signal_magnitude = abs(spins_xy);
-relative_signal = 100 * (1 - signal_magnitude(1,:)./ signal_magnitude(2,:));
+relative_signal = 100 * (1 - signal_magnitude(:,1)./ signal_magnitude(:,2));
 
 disp(relative_signal')
 
-vessel_radius = [0.5 0.8 1 1.5 2 4 5.5 8 10 12 16 20 32 45 64 75 90 100 110 128 140 180 256 400 512 620 800 1024];
+vessel_radius = rad_ref_um * scales;
 
 semilogx(vessel_radius, relative_signal); xlabel('Vessel radius (um)'); ylabel('Relative Signal %');
-
-%% 
-
+hold on
