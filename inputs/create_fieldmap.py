5569a02e3e8e3fa07a0414ac2c2a45e8724828ac
Ali Aghaeifar <ali.aghaeifar@tuebingen.mpg.de>
2023-03-13 17:27:41 +0100

    a few bugs fixed

--- inputs/create_fieldmap.py
+++ inputs/create_fieldmap.py
@@ -17,11 +17,11 @@ FoV  = np.asarray([1e3, 1e3, 1e3]) # Field of view in um
 Res  = np.asarray([256, 256, 256]) # Resolution in voxels
 FoV2 = (FoV - FoV/Res) / 2 # update Field of view to account for the voxel size and center the field map
 
-is_orientation_random = True 
+is_orientation_random = False 
 theta_B0 = np.pi/2     # angle between axis of vessel and B0 if is_orientation_random == True
 Y     = 0.75  # oxygenation level of blood
 Htc   = 0.4   # hematocrit level
-dChi  = 0.273 # susceptibility difference between fully deoxygeneted blood and tissue
+dChi  = 0.273e-6 # susceptibility difference between fully deoxygeneted blood and tissue
 BV    = 0.03  # blood volume fraction
 vessel_radius = 50 # vessel diameter in um
 
@@ -30,14 +30,14 @@ vessel_radius = 50 # vessel diameter in um
 xv, yv, zv = np.meshgrid(np.linspace(-FoV2[0], FoV2[0], Res[0], endpoint=True), 
                          np.linspace(-FoV2[1], FoV2[1], Res[1], endpoint=True),
                          np.linspace(-FoV2[2], FoV2[2], Res[2], endpoint=True), indexing='ij')
-p3 = np.vstack((xv.flatten(order='F'), yv.flatten(order='F'), zv.flatten(order='F'))).T
+p3 = np.vstack((xv.flatten(order='F'), yv.flatten(order='F'), zv.flatten(order='F')), dtype=np.float32).T
 xv = None  # save memory
 yv = None
 zv = None
 
 
 # create a fieldmap
-dW = np.zeros(p3.shape[0])
+dW = np.zeros(p3.shape[0], dtype=np.float32)
 vessel_mask = np.zeros(Res, dtype=bool).flatten(order='F')
 
 while True:
@@ -61,7 +61,7 @@ while True:
     # d = np.linalg.norm(np.cross(p2-p1,p3-p1), axis=1)/np.linalg.norm(p2-p1) # distance to vessel
 
     # reference vector of current cylinder (vessel), a vector prependicular to the cylinder axis and its Y element is zero
-    v2_u = np.asarray([1., 0., 0.], dtype = float) # suppose this is a vector of form [1,0,x], connects point P3 to p1
+    v2_u = np.asarray([1., 0., 0.], dtype = np.float32) # suppose this is a vector of form [1,0,x], connects point P3 to p1
     v2_u[2] = -v2_u[0] * p21[0]/p21[2] # update the x (3rd) element of the v2_u. We did a dot product between v and p21 to get the x element of v2_u. Dot product should be zero since two vectors are perpendicular. We can solve for x element of v2_u.
     cos2theta = np.divide(np.dot(v1_u, v2_u), np.linalg.norm(v1_u, axis=1)) / np.linalg.norm(v2_u)
     cos2theta = cos2theta * cos2theta
@@ -69,7 +69,7 @@ while True:
     dW_temp = 2*np.pi*(1-Y)*dChi*Htc * (vessel_radius**2) / (d**2) * (sin(theta_B0)**2) * (2*cos2theta - 1)
     f_in = 2*np.pi*(1-Y)*dChi*Htc * (cos(theta_B0)**2 - 1/3)
         
-    mask_local = np.zeros_like(vessel_mask) # maks for the current vessel
+    mask_local = np.zeros_like(vessel_mask, dtype = bool) # maks for the current vessel
     np.putmask(mask_local, (d < vessel_radius), True)  
     vessel_mask = np.logical_or(vessel_mask, mask_local) # expand vessel mask
     
@@ -80,7 +80,10 @@ while True:
     BV_t = np.sum(vessel_mask) / np.prod(Res)
     if BV_t > BV:
         break
-    
+   
 dW = dW.reshape(Res)
 vessel_mask = vessel_mask.reshape(Res)
-sp.io.savemat('fieldmap.mat', mdict={'fieldmap': dW, 'mask': vessel_mask})
+filename = 'fieldmap.mat'
+if is_orientation_random == False:
+    filename = 'fieldmap_ori=' + "{:.1f}".format(theta_B0*180/np.pi) + '.mat'
+sp.io.savemat(filename, mdict={'fieldmap': dW, 'mask': vessel_mask, 'FoV': FoV*1e-6, 'Res': Res, 'vessel_radius': vessel_radius*1-6, 'BV': BV, 'Y': Y})
