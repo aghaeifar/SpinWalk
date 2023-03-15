d0e224a5f9ca0c2ac40cb270adf6775b088ee249
Ali Aghaeifar <ali.aghaeifar@tuebingen.mpg.de>
2023-03-15 11:46:35 +0100

    missing << added

--- common/miscellaneous.h
+++ common/miscellaneous.h
@@ -95,7 +95,7 @@ bool save_output(std::vector<float> &data, std::string output_filename, output_h
     std::filesystem::path parent_path = std::filesystem::absolute(output_filename).parent_path();
     if (std::filesystem::is_directory(parent_path) == false)
     {
-        std::cout << ERR_MSG << "cannot find directory " << parent_path.string() << ". Trying to create it." std::endl;
+        std::cout << ERR_MSG << "cannot find directory " << parent_path.string() << ". Trying to create it." << std::endl;
         if(std::filesystem::create_directories(parent_path))
         {
             std::cout << ERR_MSG << "cannot create directory " << parent_path.string() << std::endl;
