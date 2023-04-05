df5bd6a3654029bd4fcc9d39fe645a3254004093
Ali Aghaeifar <ali.aghaeifar@tuebingen.mpg.de>
2023-04-05 12:39:09 +0200

    phase cycling + multiecho

--- common/kernels.h
+++ common/kernels.h
@@ -12,6 +12,7 @@
 #define _KERNELS_H_
 
 #include <numeric>
+#include <algorithm>
 #include "miscellaneous.h"
 #include "rotation.h"
 
@@ -33,6 +34,19 @@ uint64_t sub2ind(uint32_t x, uint32_t y, uint32_t z, uint32_t lenx, uint32_t len
     return (uint64_t(z*lenx*leny) + y*lenx + x);
 }
 
+template <typename T>
+#ifdef __CUDACC__
+__device__ __forceinline__
+#endif
+uint16_t find_ind(const T *arr, uint16_t len, T seek)
+{
+    for(uint16_t i=0; i<len; i++)
+        if (arr[i] == seek)
+            return i;
+    return len;
+}
+
+
 #ifdef __CUDACC__
 __global__ 
 #endif
@@ -52,8 +66,8 @@ void simulation_kernel(const simulation_parameters *param, const float *pFieldMa
     std::normal_distribution<float> dist_random_walk_xyz(0.f, sqrt(6 * param->diffusion_const * param->dt));
 #endif
 
-    uint16_t n_timepoints_local;
-    float accumulated_phase = 0., field = 0.; 
+    //uint16_t n_timepoints_local;
+    float accumulated_phase = 0., field = 0., phase_cycle = 0., time_elapsed = 0.; 
     float m0[3], m1[3]; 
     float xyz[3], xyz_new[3];
     for(uint32_t i=0, shift=3*spin_no; i<3; i++)
@@ -65,7 +79,8 @@ void simulation_kernel(const simulation_parameters *param, const float *pFieldMa
     // -alpha/2 RF pulse (along x-axis) + TR/2 relaxation
     if (param->enApplyFA2)
     {
-        xrot(-param->s2, param->c2, m0, m1); // note this is -FA/2
+        //xrot(-param->s2, param->c2, m0, m1); // note this is -FA/2
+        xrot_phasecycled (param->s2, param->c2, phase_cycle += param->phase_cycling, m0, m1);
         relax(param->e12, param->e22, m1, m0);
     }
 
@@ -73,11 +88,17 @@ void simulation_kernel(const simulation_parameters *param, const float *pFieldMa
     for (uint32_t dummy_scan = 0; dummy_scan < param->n_dummy_scan + 1; dummy_scan++)
     {
         is_lastdummy = (dummy_scan == param->n_dummy_scan);
-        n_timepoints_local = is_lastdummy ? (param->n_timepoints) / 2 : (param->n_timepoints); // random walk till TR/2 in the final execution of the loop
+        //n_timepoints_local = is_lastdummy ? (param->n_timepoints) / 2 : (param->n_timepoints); // random walk till TR/2 in the final execution of the loop
         
-        // alpha RF pulse (along x-axis) + TR or TR/2 relaxation
-        float s_cycl = (dummy_scan % 2 == 0) ? param->s : -param->s; // PI phase cycling, starts with +FA (since we have -FA/2 above as the first pulse)
-        xrot(s_cycl, param->c, m0, m1);
+        //alpha RF pulse (along x-axis) + TR or TR/2 relaxation
+        //float s_cycl = (dummy_scan % 2 == 0) ? param->s : -param->s; // PI phase cycling, starts with +FA (since we have -FA/2 above as the first pulse)
+        //xrot(s_cycl, param->c, m0, m1);
+        if (phase_cycle > M_2PI)
+            phase_cycle -= M_2PI;
+        else if (phase_cycle < 0)
+            phase_cycle += M_2PI;
+
+        xrot_phasecycled (param->s, param->c, phase_cycle += param->phase_cycling, m0, m1);
 
         // copy m1 to m0
         for(uint8_t i=0; i<3; i++)
@@ -85,11 +106,12 @@ void simulation_kernel(const simulation_parameters *param, const float *pFieldMa
 
         // random walk with boundries and accomulate phase
         uint64_t ind=0, ind_old=param->matrix_length+1;
-        uint16_t current_timepoint = 0;
+        uint16_t current_timepoint = 0, old_timepoint = 0;
         accumulated_phase = 0;
 
-        while (current_timepoint < n_timepoints_local)
+        while (current_timepoint < param->n_timepoints)
         {
+            // random walk
             for (uint8_t i=0; i<3; i++)
             {
                 xyz_new[i] = xyz[i] + dist_random_walk_xyz(gen); // new spin position after random-walk
@@ -101,38 +123,73 @@ void simulation_kernel(const simulation_parameters *param, const float *pFieldMa
             // subscripts to linear indices
             ind = sub2ind(ROUND(xyz_new[0]*param->scale2grid[0]), ROUND(xyz_new[1]*param->scale2grid[1]), ROUND(xyz_new[2]*param->scale2grid[2]), param->fieldmap_size[0], param->fieldmap_size[1]);
             //accumulate phase
-            if(ind != ind_old) // used this trick to access fewer to the global memorsy which is slow. Helpful for big samples!
+            if(ind != ind_old) // used this trick for fewer access to the global memory which is slow. Helpful for big samples!
             {               
                 if (pMask[ind] == true) // check doesn't cross a vessel 
                     continue;       
                 field = pFieldMap[ind];
                 ind_old = ind; 
-            }
-     
+            }     
             accumulated_phase += field;
-            if(param->enRefocusing180 && current_timepoint == param->n_timepoints/4)
-                accumulated_phase *= -1; // 180 degree refocusing pulse
+
+            // apply refocusing pulse 
+            if(param->enRefocusing)
+            {
+                uint16_t ind = find_ind<uint16_t>(param->T_SE, param->n_SE, current_timepoint);                
+                if (ind < param->n_SE)
+                {   
+                    // dephase                 
+                    accumulated_phase *= param->B0 * GAMMA * param->dt; // Fieldmap per Tesla to radian    
+                    zrot(accumulated_phase, m0, m1); 
+                    // relax
+                    time_elapsed = (current_timepoint - old_timepoint) * param->dt;
+                    relax(exp(-time_elapsed/param->T1), exp(-time_elapsed/param->T2), m1);
+                    // refocusing pulse
+                    xrot(param->RF_SE[ind], m1, m0); // Note m0 and m1 are swapped here, so that we can use m0 for the next iteration
+                    accumulated_phase = 0; // reset phase since we have applied it in the previous step
+                    old_timepoint = current_timepoint;
+                }
+            }
+
+            // record echo, only in the last scan
+            if (is_lastdummy)
+            {
+                uint16_t ind = find_ind<uint16_t>(param->TE, param->n_TE, current_timepoint);
+                if (ind < param->n_TE)
+                {   
+                    // dephase                  
+                    accumulated_phase *= param->B0 * GAMMA * param->dt; // Fieldmap per Tesla to radian    
+                    zrot(accumulated_phase, m0, m1); 
+                    // relax                    
+                    time_elapsed = (current_timepoint - old_timepoint) * param->dt;
+                    relax(exp(-time_elapsed/param->T1), exp(-time_elapsed/param->T2), m1);
+                    // save echo and copy m1 to m0 for the next iteration
+                    for (uint32_t i=0, shift=3*param->n_TE*spin_no + 3*ind; i<3; i++)
+                        M1[shift + i] = m0[i] = m1[i];   
+                    accumulated_phase = 0; // reset phase since we have applied it in the previous step
+                    old_timepoint = current_timepoint;
+                }
+            }
 
             for (uint8_t i = 0; i < 3; i++)
                 xyz[i] = xyz_new[i];
  
             current_timepoint++;            
         }
-        
+        // dephase
         accumulated_phase *= param->B0 * GAMMA * param->dt; // Fieldmap per Tesla to radian     
         zrot(accumulated_phase, m0, m1); // dephase
-        relax(is_lastdummy ? param->e12 : param->e1, is_lastdummy ? param->e22 : param->e2, m1);
+        // relax
+        time_elapsed = (current_timepoint - old_timepoint) * param->dt;
+        relax(exp(-time_elapsed/param->T1), exp(-time_elapsed/param->T2), m1);
 
         // copy m1 to m0 for the next iteration
         for(uint8_t i=0; i<3; i++)
             m0[i] = m1[i];
     }
-
+    // save final position
     for (uint32_t i=0, shift=3*spin_no; i<3; i++)
-    {
         XYZ1[shift + i] = xyz[i];
-        M1[shift + i]   = m1[i];   
-    }
 }
 
 #ifdef __CUDACC__
@@ -212,16 +269,22 @@ void simulate_steady_state(simulation_parameters param)
         relax (param.e12, param.e22, m0_init);
     }
 
-    float m0t[3], m1t[3], s_cycl;
+    float m0t[3], m1t[3];
     std::copy (m0_init, m0_init + 3, m0t);
     std::cout << "Steady-state report:" << std::endl;
     std::cout << "\tM0 after alpha/2 pulse & TR/2 relaxation = [" << m0t[0]
               << " " << m0t[1] << " " << m0t[2] << "]" << std::endl;
+    float phase_cycle = 0;
     for (uint32_t rep = 0; rep < 3; rep++)
         for (uint32_t dummy_scan = 0; dummy_scan < param.n_dummy_scan; dummy_scan++)
         {
-            s_cycl = (dummy_scan % 2 == 0) ? -param.s : param.s;
-            xrot (s_cycl, param.c, m0t, m1t);
+            // s_cycl = (dummy_scan % 2 == 0) ? -param.s : param.s;
+            if (phase_cycle > M_2PI)
+                phase_cycle -= M_2PI;
+            else if (phase_cycle < 0)
+                phase_cycle += M_2PI;
+            xrot_phasecycled (param.s, param.c, phase_cycle += param.phase_cycling, m0t, m1t);
+
             if (dummy_scan != param.n_dummy_scan - 1)
                 relax (param.e1, param.e2, m1t);
             else
