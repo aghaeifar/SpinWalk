5baf640b9e314fa7e88b369c0413403ce2f0beee
Ali Aghaeifar <ali.aghaeifar@tuebingen.mpg.de>
2023-04-05 12:38:55 +0200

    phase cycled rotation

--- common/rotation.h
+++ common/rotation.h
@@ -59,14 +59,68 @@ __host__  __device__ __forceinline__
 #else
 inline
 #endif
+void zrot(float sin_theta, float cos_theta, float *m0, float *m1)
+{
+    m1[0] = cos_theta*m0[0] - sin_theta*m0[1];
+    m1[1] = sin_theta*m0[0] + cos_theta*m0[1];
+    m1[2] = m0[2];
+}
+
+#ifdef __CUDACC__
+__host__  __device__ __forceinline__
+#else
+inline
+#endif
 void zrot(float theta, float *m0, float *m1)
 {
+    if(theta == 0.0f)
+    {
+        m1[0] = m0[0];
+        m1[1] = m0[1];
+        m1[2] = m0[2];
+        return;
+    }
     float s = sinf(theta);
     float c = cosf(theta);
     m1[0] = c*m0[0] - s*m0[1];
     m1[1] = s*m0[0] + c*m0[1];
     m1[2] = m0[2];
 }
+
+
+#ifdef __CUDACC__
+__host__  __device__ __forceinline__
+#else
+inline
+#endif
+void xrot_phasecycled(float sin_theta, float cos_theta, float phase_cycling, float *m0, float *m1)
+{
+    if (phase_cycling == 0.0f)
+    {
+        xrot(sin_theta, cos_theta, m0, m1);
+        return;
+    }
+
+    if (phase_cycling == M_PI)
+    {
+        xrot(-sin_theta, cos_theta, m0, m1);
+        return;
+    }
+
+    float m1_t[3];
+    float s = sinf(phase_cycling);
+    float c = cosf(phase_cycling);    
+    zrot(-s, c, m0, m1);
+    xrot(sin_theta, cos_theta, m1, m1_t);
+    zrot(s, c, m1_t, m1);
+    // MATLAB code:
+    // fa = pi/5;
+    // v = [1, 3, 0];
+    // disp(axang2rotm([v, fa]))
+    // P = atan2d(v(2), v(1));
+    // disp(zrot(P) * xrot(rad2deg(fa)) * zrot(-P));
+}
+
 
 #ifdef __CUDACC__
 __host__  __device__ __forceinline__
