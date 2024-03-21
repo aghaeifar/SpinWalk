8444bb50d1291566167bbbe4599f489b7e3f3622
Ali Aghaeifar <ali.aghaeifar@tuebingen.mpg.de>
2024-03-21 13:52:44 +0100

    fixed

--- tests/ut.sh
+++ tests/ut.sh
@@ -1,1 +1,10 @@
-spinwalk -c ./../config/gre.ini ./../config/se.ini ./../config/gre.ini ./../config/ssfp.ini ./../config/grase.ini ./../config/stimulated_echo.ini ./../config/gre_save_trajectory.ini 
+#!/bin/bash
+
+# Get the directory where the script is located
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Change to the script directory
+pushd "$SCRIPT_DIR" > /dev/null
+
+spinwalk -c ./../config/gre.ini  ./../config/se.ini ./../config/ssfp.ini ./../config/grase.ini ./../config/stimulated_echo.ini ./../config/gre_save_trajectory.ini 
+
