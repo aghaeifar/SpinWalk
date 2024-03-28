e2d5714385201166fa24b786148d09f1a0d289ab
Ali Aghaeifar <ali.aghaeifar@tuebingen.mpg.de>
2024-03-28 11:05:18 +0100

    update

--- MATLAB/write_fieldmap.m
+++ MATLAB/write_fieldmap.m
@@ -1,9 +1,12 @@
 function write_fieldmap(fieldmap, mask, fov, filename)
 
 fileID = fopen(filename, 'w');
-fwrite(fileID, size(fieldmap), 'int32'); %this is the matrix size
+
+fwrite(fileID, size(mask), 'int32'); %this is the matrix size
 fwrite(fileID, single(fov), 'single');% FOV in [m]
-fwrite(fileID, single(fieldmap), 'single'); % dB0 map
+if isempty(fieldmap) == false
+    fwrite(fileID, single(fieldmap), 'single'); % dB0 map
+end
 fwrite(fileID, uint8(mask), 'uint8'  ); % mask of vessels
 fclose(fileID);
 
