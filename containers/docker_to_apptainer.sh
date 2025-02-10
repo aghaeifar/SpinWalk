658b3c4ae79659b6456c0b9c9c5b5b6baae1af70
Ali Aghaeifar <ali.aghaeifar@tuebingen.mpg.de>
2025-02-10 10:50:09 +0100

    minor

--- containers/docker_to_apptainer.sh
+++ containers/docker_to_apptainer.sh
@@ -8,10 +8,9 @@ set -e
 # Get a list of Docker images starting with "spinwalk"
 images=$(docker images --format "{{.Repository}}:{{.Tag}}" | grep "^spinwalk")
 TAR_FILE="/DATA2/spinwalk.tar"
-
 for image in $images; do
     IMAGE_NAME=$image    
-    APPTAINER_IMAGE="/DATA2/$image.sif"
+    APPTAINER_IMAGE="/DATA2/${image//:/_}.sif"
     # Save the Docker image to a tar file
     echo "Saving Docker image to $TAR_FILE..."
     docker save -o $TAR_FILE $IMAGE_NAME
